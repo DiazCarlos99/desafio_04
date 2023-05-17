386f67dcb0fe2b2fd37ecabe0c181df6887334c2
carlosdiaz <carlosrdiaz100@gmail.com>
2023-05-17 11:51:47 -0300

    primera parte

--- desafio04/desafio04.py
+++ desafio04/desafio04.py
@@ -1,1 +1,35 @@
- 
+from function import mostrar_menu, mostrar_inmuebles, editar_inmueble, buscar_inmuebles_por_presupuesto, cambiar_estado, eliminar_inmueble, agregar_inmueble
+
+
+inmuebles = [{'año': 2010, 'metros': 150, 'habitaciones': 4, 'garaje': True, 'zona': 'C', 'estado': 'Disponible'},
+             {'año': 2016, 'metros': 80, 'habitaciones': 2,
+                 'garaje': False, 'zona': 'B', 'estado': 'Reservado'},
+             {'año': 2000, 'metros': 180, 'habitaciones': 4,
+                 'garaje': True, 'zona': 'A', 'estado': 'Disponible'},
+             {'año': 2015, 'metros': 95, 'habitaciones': 3,
+                 'garaje': True, 'zona': 'B', 'estado': 'Vendido'},
+             {'año': 2008, 'metros': 60, 'habitaciones': 2, 'garaje': False, 'zona': 'C', 'estado': 'Disponible'}]
+
+while True:
+    mostrar_menu()
+    opcion = input('Ingrese un opción: ')
+    if not opcion:
+        print(f'Ingrese una opción numerica valida')
+        continue
+
+    if opcion == '1':
+        mostrar_inmuebles(inmuebles)
+    elif opcion == '2':
+        agregar_inmueble(inmuebles)
+    elif opcion == '3':
+        editar_inmueble(inmuebles)
+    elif opcion == '4':
+        eliminar_inmueble(inmuebles)
+    elif opcion == '5':
+        cambiar_estado(inmuebles)
+    elif opcion == '6':
+        buscar_inmuebles_por_presupuesto(inmuebles)
+    elif opcion == '7':
+        break
+    else:
+        print("Opción inválida. Intente nuevamente.")
---
+++ desafio04/function.py
@@ -0,0 +1,25 @@
+def mostrar_menu():
+    print(f'--------------- Menú ---------------')
+    print(f'1. Mostrar lista de inmuebles')
+    print("2. Agregar inmueble")
+    print("3. Editar inmueble")
+    print("4. Eliminar inmueble")
+    print("5. Cambiar estado de inmueble")
+    print("6. Buscar inmuebles por presupuesto")
+    print("7. Salir")
+    print("-----------------------------")
+
+def mostrar_inmuebles(inmuebles):
+    print(f'')
+def agregar_inmueble(inmuebles):
+    print(f'')
+def editar_inmueble(inmuebles):
+    print(f'')
+def agregar_inmueble(inmuebles):
+    print(f'')
+def eliminar_inmueble(inmuebles):
+    print(f'')
+def cambiar_estado(inmuebles):
+    print(f'')
+def buscar_inmuebles_por_presupuesto(inmuebles):
+    print(f'')
